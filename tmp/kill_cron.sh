7b26d4aa6edc0a99fd506dc3b66b1c8d6ae11505
Sebastijan <sebastijan.trojer1@gmail.com>
2024-06-27 13:33:32 +0200

    Bind shutdown to CTRL + Shift + D

--- tmp/kill_cron.sh
+++ tmp/kill_cron.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-pgid_path="$HOME/trust-me-setup/tmp/startup_script.pid"
+pgid_path="/home/dis/trust-me-setup/tmp/startup_script.pid"
+
+echo $(ls /home/dis/trust-me-setup/tmp)
 
 # Read the PID from the file
 if [ -f "$pgid_path" ]; then
     PGID=$(cat "$pgid_path")
     echo "Sending SIGINT to PGID $PGID"
     kill -SIGINT -- -$PGID
-    rm "$pgid_path"  # Clean up the PID file
 else
     echo "PGID file not found. Is your_script.sh running?"
 fi
 
+sleep 10
+
+echo "Shutting down..."
