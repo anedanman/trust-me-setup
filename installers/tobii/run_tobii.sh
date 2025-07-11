0f281c64a57e8f4fdb03682a9781f033e594b7ac
Daniil Kirilenko <60619274+anedanman@users.noreply.github.com>
2025-07-11 13:56:51 +0200

    Update run_tobii.sh

--- installers/tobii/run_tobii.sh
+++ installers/tobii/run_tobii.sh
@@ -2,8 +2,8 @@
 
 # Define base path
 BASE_PATH="/home/$(whoami)/trust-me-setup"
-PID_DIR="$BASE_PATH/tmp/pids/tobiish"
-echo $$ > "$PID_DIR"
+# PID_DIR="$BASE_PATH/tmp/pids/tobiish"
+# echo $$ > "$PID_DIR"
 
 # Get username from parameter or file
 USERNAME=${1:-$(cat "$BASE_PATH/tmp/current_username" 2>/dev/null || echo "user1")}
